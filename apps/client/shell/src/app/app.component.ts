a7200a9f36fadcbcfdaccbf4b0e2e6704dc76a72
elhakimdev99 <abdulelhakim68@gmail.com>
2026-01-31 23:26:44 +0700

    fix: fixing homeland

--- apps/client/shell/src/app/app.component.ts
+++ apps/client/shell/src/app/app.component.ts
@@ -1,11 +1,45 @@
 import { Component, signal } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import appComponentTemplate from "./app.component.html";
 import { AsyncPipe } from "@angular/common";
+import appComponentTemplate from "./app.component.html";
+
 @Component({
   selector: "app-root",
   template: appComponentTemplate,
-  styleUrls: ["./app.component.scss"],
+  styles: [
+    ` :host {
+      font-family: 'Inter', sans-serif;
+    }
+
+    .animate-fade-in-down {
+      animation: fadeInDown 0.8s cubic-bezier(0.16, 1, 0.3, 1);
+    }
+    .animate-fade-in-up {
+      animation: fadeInUp 0.8s cubic-bezier(0.16, 1, 0.3, 1) backwards;
+    }
+
+    @keyframes fadeInDown {
+      from {
+        opacity: 0;
+        transform: translateY(-40px);
+      }
+      to {
+        opacity: 1;
+        transform: translateY(0);
+      }
+    }
+    @keyframes fadeInUp {
+      from {
+        opacity: 0;
+        transform: translateY(40px);
+      }
+      to {
+        opacity: 1;
+        transform: translateY(0);
+      }
+    }
+    `
+  ],
   standalone: true,
   imports: [RouterOutlet, AsyncPipe]
 })
@@ -16,5 +50,5 @@ export class AppComponent {
   increment() {
     this.counter.update((value) => value + 1);
     console.log('counter value:', this.counter());
-  } 
+  }
 }
--- apps/client/shell/src/types.d.ts
+++ apps/client/shell/src/types.d.ts
@@ -1,11 +1,11 @@
-// // Mengajarkan TS cara baca file HTML
-// declare module '*.html' {
-//   const content: string;
-//   export default content;
-// }
+// Mengajarkan TS cara baca file HTML
+declare module '*.html' {
+  const content: string;
+  export default content;
+}
 
-// // (Opsional) Mengajarkan TS cara baca SCSS (kalau nanti mau import style juga)
-// declare module '*.scss' {
-//   const content: string;
-//   export default content;
-// }
+// (Opsional) Mengajarkan TS cara baca SCSS (kalau nanti mau import style juga)
+declare module '*.scss' {
+  const content: string;
+  export default content;
+}
